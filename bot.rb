efb271e30e4ac297662e0686e2404660fa657d56
Meme <meme_es@hotmail.com>
2020-04-12 10:23:45 -0600

    Add content to the bot.rb file
    This file content the WhatsAppBot class wich is the gateway to interact with our bot.
    The #post method recibe an inbound http request and answer sending a message.

--- bot.rb
+++ bot.rb
@@ -1,7 +1,26 @@
 require 'sinatra/base'
+require './lib/animals'
 
 class WhatsAppBot < Sinatra::Base
-  get '/' do
-    'Hello World!'
+  use Rack::TwilioWebhookAuthentication, ENV['TWILIO_AUTH_TOKEN'], '/bot'
+
+  post '/bot' do
+    body = params['Body'].downcase
+    response = Twilio::TwiML::MessagingResponse.new
+    response.message do |message|
+      if body.include?('dog')
+        message.body(Dog.fact)
+        message.media(Dog.picture)
+      end
+
+      if body.include?('cat')
+        message.body(Cat.fact)
+        message.media(Cat.picture)
+      end
+
+      message.body('I only know about dogs or cats, sorry!') unless body.include?('dog') || body.include?('cat')
+    end
+    content_type 'text/xml'
+    response.to_xml
   end
 end
